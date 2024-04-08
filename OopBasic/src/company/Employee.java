4a71fb1df3cdcfe9051c576e62c454639f4d153c
dir-FY24shomatsunozaki <shoma.tsunozaki.t825@dirbato.co.jp>
2024-04-08 11:06:00 +0900

    Lesson3.8

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,13 +1,13 @@
 package company;
 
 public class Employee {
-    // フィールド
+    
     protected final String name;
     private final Department department;
     private final String position;
     private final int employeeId;
 
-    // コンストラクター
+    
     public Employee(String name, Department department, String position, int employeeId) {
         this.name = name;
         this.department = department;
@@ -15,17 +15,17 @@ public class Employee {
         this.employeeId = employeeId;
     }
 
-    // 報告メソッド
+    
     public void report(int times) {
         System.out.println(times + "回目の報告をします。役職：" + position + "、名前：" + name);
     }
 
-    // 報告メソッド（オーバーロード）
+    
     public void report() {
         report(1);
     }
     
-    //会議参加メソッド
+    
     public void joinMeeting() {
         department.meeting();
         System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
